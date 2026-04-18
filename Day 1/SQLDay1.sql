ce32cf69d613e32b899352c6f135e6f729e362ee
Meezan Shaikh <meezanraza1803@gmail.com>
2026-04-18 16:51:41 +0530

    Updated SQLDay1 file

--- Day 1/SQLDay1.sql
+++ Day 1/SQLDay1.sql
@@ -17,4 +17,40 @@ insert into Employee values (4, 'Zoya', 24, 'Female', 28000, 'HR', 'Pune')
 insert into Employee values (5, 'Julia', 22, 'Female', 26000, 'Dr', 'Nashik') 
 
 --Select Everything from table Employee / to show table
+select * from Employee 
+
+-- If we've only 2 / 3 column information then we write
+insert into Employee (Int, Name, Gender, City) values
+(6, 'Parth', 'Male', 'Surat')
+
+-- Update data
+update Employee set salary = 35000 where name = 'Parth'
+
+--Delete data
+delete from Employee where name = 'Zoya'
+
 select * from Employee
+
+
+
+
+
+-- For Practice creating a new table
+Create table Student
+(id int, Roll int, Name varchar(255), Gender varchar(255))
+
+insert into Student values (1, 23, 'Sarthak', 'Male')
+insert into Student values (2, 24, 'Sahili', 'Female')
+insert into Student values (3, 25, 'Zoya', 'Female')
+insert into Student values (4, 26, 'Moin', 'Male')
+insert into Student values (5, 27, 'Junaid', 'Male')
+
+select * from Student
+
+
+-- Update data from table Student 
+update Student set Roll = 10 where name = 'Sarthak'
+update Student set Name = 'Julia' where name = 'Zoya'
+
+select * from Student
+
